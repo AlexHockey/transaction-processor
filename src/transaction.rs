27e772c0f2b74916c027c85d8c9f43a8a8f781d0
Alex Hockey <alex.j.hockey@gmail.com>
2024-07-30 14:01:10 +0100

    Add tests for transaction log parsing

--- src/transaction.rs
+++ src/transaction.rs
@@ -20,6 +20,7 @@ struct Record {
 
 /// Struct representing a single transaction. All transactions have a id and reference a client.
 /// Some also have type-specific fields.
+#[derive(Debug)]
 pub struct Transaction {
     pub id: u32,
     pub client: u16,
@@ -27,6 +28,7 @@ pub struct Transaction {
 }
 
 /// The different types of operations that transactions can represent, plus any associated data.
+#[derive(Debug)]
 pub enum Operation {
     Deposit(f64),
     Withdrawal(f64),
@@ -65,6 +67,10 @@ impl TryFrom<Record> for Transaction {
 pub fn iter_over_file(
     file_path: &str,
 ) -> Result<impl Iterator<Item = Transaction>, Box<dyn Error>> {
+    Ok(iter_over_reader(std::fs::File::open(file_path)?))
+}
+
+fn iter_over_reader<R>(reader: R) -> impl Iterator<Item = Transaction> where R: std::io::Read {
     // Build a reader.
     // - The CSV has a header we need to strip.
     // - The CSV has variable numbers of columns so we need `flexible` to be set.
@@ -73,14 +79,104 @@ pub fn iter_over_file(
         .has_headers(true)
         .flexible(true)
         .trim(Trim::All)
-        .from_path(file_path)?;
+        .from_reader(reader);
 
     // Deserailize, skipping any errors.
     // TODO: Add logging when encountering errors.
-    let it = rdr
+    rdr
         .into_deserialize()
         .filter_map(|elem| elem.ok())
-        .filter_map(|rec: Record| rec.try_into().ok());
+        .filter_map(|rec: Record| rec.try_into().ok())
+}
 
-    Ok(it)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mainline_parsing() {
+        let input = r"type, client, tx, amount
+deposit, 1, 1, 2.0
+withdrawal, 1, 2, 1.0
+dispute, 1, 1
+resolve, 1, 1
+chargeback, 1, 1
+";
+
+        let mut it = iter_over_reader(input.as_bytes());
+
+        let tx = it.next().unwrap();
+        assert_eq!(tx.id, 1);
+        assert_eq!(tx.client, 1);
+        assert!(matches!(tx.op, Operation::Deposit(2.0)));
+
+        let tx = it.next().unwrap();
+        assert_eq!(tx.id, 2);
+        assert_eq!(tx.client, 1);
+        assert!(matches!(tx.op, Operation::Withdrawal(1.0)));
+
+        let tx = it.next().unwrap();
+        assert_eq!(tx.id, 1);
+        assert_eq!(tx.client, 1);
+        assert!(matches!(tx.op, Operation::Dispute));
+
+        let tx = it.next().unwrap();
+        assert_eq!(tx.id, 1);
+        assert_eq!(tx.client, 1);
+        assert!(matches!(tx.op, Operation::Resolve));
+
+        let tx = it.next().unwrap();
+        assert_eq!(tx.id, 1);
+        assert_eq!(tx.client, 1);
+        assert!(matches!(tx.op, Operation::Chargeback));
+
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_bad_type() {
+        let input = r"type, client, tx, amount
+incorrect, 1, 1, 2.0
+";
+
+        let mut it = iter_over_reader(input.as_bytes());
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_no_amount_on_deposit() {
+        let input = r"type, client, tx, amount
+deposit, 1, 1
+";
+
+        let mut it = iter_over_reader(input.as_bytes());
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_no_amount_on_withdrawal() {
+        let input = r"type, client, tx, amount
+withdrawal, 1, 1
+";
+
+        let mut it = iter_over_reader(input.as_bytes());
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_bad_records_are_skipped() {
+        let input = r"type, client, tx, amount
+withdrawal, 1, 1
+withdrawal, 1, 1, 1.0
+withdrawal, 1, 1
+";
+
+        let mut it = iter_over_reader(input.as_bytes());
+        let tx = it.next().unwrap();
+        assert_eq!(tx.id, 1);
+        assert_eq!(tx.client, 1);
+        assert!(matches!(tx.op, Operation::Withdrawal(1.0)));
+
+        assert!(it.next().is_none());
+    }
 }
